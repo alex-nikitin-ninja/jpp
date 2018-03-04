dd977fe8892236bcd0030f2d82155a7d746a7cba
Alex Nikitin <alex@nikitin.ninja>
2018-03-03 22:40:47 -0800

    minor update

--- jpp.go
+++ jpp.go
@@ -3,18 +3,21 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	rawJson, err := reader.ReadString('\n')
-
-	if err != nil {
-		fmt.Println("Error:")
-		fmt.Print(err)
-		os.Exit(1)
-	} else {
-		fmt.Print(rawJson)
+	for {
+		if c, _, err := reader.ReadRune(); err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				fmt.Print(err)
+			}
+		} else {
+			fmt.Print(string(c))
+		}
 	}
 }

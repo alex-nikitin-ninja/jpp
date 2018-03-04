359fccc5b90a1626f9179a8b6fa975281383d23a
Alex Nikitin <alex@nikitin.ninja>
2018-03-03 23:40:06 -0800

    minor update

--- jpp.go
+++ jpp.go
@@ -5,10 +5,24 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+	indentSymbol := "    "
+	indentCurrent := 0
+	textOpen := false
+	prevChar := " "
+
+	indentKeys := map[string][]string{
+		"indent":        []string{"{", "["},
+		"unindent":      []string{"}", "]"},
+		"newLine":       []string{","},
+		"space":         []string{":"},
+		"textEnclosure": []string{"\""},
+	}
+
 	for {
 		if c, _, err := reader.ReadRune(); err != nil {
 			if err == io.EOF {
@@ -17,7 +31,40 @@ func main() {
 				fmt.Print(err)
 			}
 		} else {
-			fmt.Print(string(c))
+
+			if r, _ := in_array(string(c), indentKeys["textEnclosure"]); r && !textOpen {
+				textOpen = true
+			} else if r, _ := in_array(string(c), indentKeys["textEnclosure"]); r && textOpen && prevChar != "\\" {
+				textOpen = false
+			}
+
+			if !textOpen {
+				if r, _ := in_array(string(c), indentKeys["indent"]); r {
+					indentCurrent++
+					fmt.Print(string(c) + "\n" + strings.Repeat(indentSymbol, indentCurrent))
+				} else if r, _ := in_array(string(c), indentKeys["unindent"]); r {
+					indentCurrent--
+					fmt.Print("\n" + strings.Repeat(indentSymbol, indentCurrent) + string(c))
+				} else if r, _ := in_array(string(c), indentKeys["newLine"]); r {
+					fmt.Print(string(c) + "\n" + strings.Repeat(indentSymbol, indentCurrent))
+				} else if r, _ := in_array(string(c), indentKeys["space"]); r {
+					fmt.Print(string(c) + " ")
+				} else {
+					fmt.Print(string(c))
+				}
+			} else {
+				fmt.Print(string(c))
+			}
+			prevChar = string(c)
 		}
 	}
+}
+
+func in_array(val string, inpArray []string) (ok bool, i int) {
+	for i = range inpArray {
+		if ok = inpArray[i] == val; ok {
+			return
+		}
+	}
+	return
 }
